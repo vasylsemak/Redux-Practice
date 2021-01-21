11a8cdab433e37b2a724ddcb13d139cc409a4e26
Vasyl Semak <vasylsmk1401@gmail.com>
2021-01-21 08:27:03 -0500

    initial commit

---
+++ src/01-redux/action-creators.js
@@ -0,0 +1,13 @@
+// you need to define the action creators below!
+
+export const adoptPet = undefined;
+
+export const previewPet = undefined;
+
+export const addNewDog = undefined;
+
+export const addNewCat = undefined;
+
+export const removeDog = undefined;
+
+export const removeCat = undefined;
---
+++ src/01-redux/reducer.js
@@ -0,0 +1,14 @@
+
+const initialState = {
+  // your code here
+};
+
+// Remember that reducers take in two parameters:
+//  1. The (previous) state. When we use 'createStore', the previous state is undefined,
+//     so, the reducer is initially invoked with undefined as the first argument.
+//     This means that if we set a default parameter value, we can use that as our initial return value
+//
+//  2. The action object, which we get whenever we use `store.dispatch`
+export default function(state = initialState, action) {
+  // your code here
+}
